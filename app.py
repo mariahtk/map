fe582be9730256b722890b76d7ac59361a8430ac
Mariah Krawchuk <78518674+mariahtk@users.noreply.github.com>
2025-07-30 17:59:05 -0400

    Update app.py
    Signed-off-by: Mariah Krawchuk <78518674+mariahtk@users.noreply.github.com>

--- app.py
+++ app.py
@@ -12,6 +12,8 @@ from branca.element import Template, MacroElement
 import os
 import tempfile
 import streamlit.components.v1 as components
+from folium import plugins
+
 
 # MUST BE FIRST Streamlit call
 st.set_page_config(page_title="Closest Centres Map", layout="wide")
@@ -235,6 +237,7 @@ if input_address:
                 closest = pd.DataFrame(selected_centres)
 
                 m = folium.Map(location=input_coords, zoom_start=14, zoom_control=True, control_scale=True)
+                plugins.ZoomControl(position='topright').add_to(m)
                 folium.Marker(location=input_coords, popup=f"Your Address: {input_address}", icon=folium.Icon(color="green")).add_to(m)
                 def get_marker_color(ftype):
                     return {"Regus":"blue","HQ":"darkblue","Signature":"purple","Spaces":"black","Non-Standard Brand":"gold"}.get(ftype,"red")
